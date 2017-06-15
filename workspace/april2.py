21f692557e7b785a179925fd73fb76bf5fe942c3
robert.tchabo <robert-patrick.tchabo-lekane@hp.com>
2017-06-15 18:08:50 +0100

    mm

--- workspace/april2.py
+++ workspace/april2.py
@@ -12,110 +12,164 @@ import bisect
  
 #pi = pi / Decimal(10**100)
 
-##http://codeforces.com/blog/entry/21693
-
-def recurs(order, nodes, edges):
-
-	node_index = 0
-	nodes[0] = {}
+def check_palin(dict):
 	
-	queue = []
-	for k in order[0]:
-		node_index += 1
-		nodes[node_index] = {}
-		nodes[0][node_index]  = k - 1  #  0 -> node_index is edge k-1 
-		edges[k-1] = (0, node_index)   # k -1 maps to 0 -> node_index, par, child
-		queue.append(k)
-	
-	
-	visited = set([0])
-	
-	while len(queue) > 0 :
-		cur = queue.pop() #dequeue ??
-	
-		new_par = edges[cur-1][1]
+	_sum = 0
+	_pres = 0
+	for i in range(1,10):
+		_pres = max( _pres, dict[i])
+		_sum += dict[i]%2
 		
-		for k in order[cur]:
-			if (k in visited):
-				continue
-			
-			node_index += 1
-			nodes[node_index] = {}
-			nodes[new_par][node_index]  = k - 1  #  new_par -> node_index is edge k-1 
-			edges[k-1] = (new_par, node_index)   # k -1 maps to 0 -> node_index, par, child
-			queue.append(k)
-		
-		visited.add(cur)
+	_sum += dict[0]%2
 	
+	return ( _sum, _pres )
+	
+	# if _sum > 1:
+		# return False
+	
+	# if _pres <=1:
+		# return False
+		
+	# return True
+	
+	
+def subs(dictA, dictB):
+	res = list(dictA)
+	
+	for i in range(10):
+		res[i] -= dictB[i]
+	
+	return res
+	
+def merge( C, A, B):
+	ptA, lA = 0, len(A)
+	ptB, lB = 0, len(B)
+	
+	pt = 0
+	while ( ptA < lA)  and ( ptB < lB ):
+		if A[ptA] <= B[ptB]:
+			C[pt] = A[ptA]
+			ptA += 1
+		else:
+			C[pt] = B[ptB]
+			ptB += 1
+		pt += 1
+		
+	for k in range(ptA,lA):
+		C[pt] = A[k]
+		pt += 1
+		
+	for k in range(ptB,lB):
+		C[pt] = B[k]
+		pt += 1
+ 
+ 
+def build_tree( tree, magicA, cur , l , r ):
+	if( l==r ):
+		tree[cur] = magicA[ l ]
+		return
+	mid = l+(r-l)/2
+	build_tree(tree, magicA, 2*cur+1 , l , mid ) #Build left tree 
+	build_tree(tree, magicA, 2*cur+2 , mid+1 , r ) #Build right tree
+	LL = 2*cur+1
+	LR = 2*cur+2
+	tree[cur] = [0] * (len(tree[LL]) + len(tree[LR]) ) #merge( tree[2*cur+1] , tree[2*cur+2] ); //Merging the two sorted arrays
+	merge(tree[cur], tree[LL], tree[LR])
+
+def query_top( magicTree, cur, l, r, x, y, k) :
+	if( r < x or l > y ):
+		return 0 #out of range
+ 
+	if( x<=l and r<=y ) : #Binary search over the current sorted vector to find elements smaller than K
+		#upper_bound(tree[cur].begin(),tree[cur].end(),K)-tree[cur].begin();
+		return bisect.bisect_right(magicTree[cur],k)
+	mid=l+(r-l)/2
+	return query_top(magicTree, 2*cur+1,l,mid,x,y,k)+query_top(magicTree, 2*cur+2,mid+1,r,x,y,k)
+	
+	
+def query_bot( magicTree, cur, l, r, x, y, k) :
+	if( r < x or l > y ):
+		return 0 #out of range
+ 
+	if( x<=l and r<=y ) : #Binary search over the current sorted vector to find elements smaller than K
+		#upper_bound(tree[cur].begin(),tree[cur].end(),K)-tree[cur].begin();
+		return bisect.bisect_left(magicTree[cur],k)
+	mid=l+(r-l)/2
+	return query_bot(magicTree, 2*cur+1,l,mid,x,y,k)+query_bot(magicTree, 2*cur+2,mid+1,r,x,y,k)
+	
+def getDict(magicTree, N, l,r,x,y):
+	
+	print query_top( magicTree, 0, 0, N-1, l, r,y) - query_bot( magicTree, 0, 0, N-1, l, r,x)
 	
 def main():
 	
 	import sys
- 
+	from collections import deque
 	
-	T = int(sys.stdin.readline().strip())
 	
-	for _ in range(T):
-		N, M = map(int , sys.stdin.readline().strip().split())
-		
-		edges = [0]*N
-		weigth = [0]*N
-		
-		nodes = {}
-		
-		order = [0]* (N+1)
-		for n in range(N+1):
-			order[n] = []
-		
-		for n in range(N):
-			P, W = map(int , sys.stdin.readline().strip().split())
-			weigth[n] = W  ## weigth of edge n
-			
-			order[n+1].append(P)
-			order[P].append(n+1)
-		
-		recurs(order, nodes, edges)
-		
-		#print nodes
-		#print edges
-		
-		
-		
-		
-		
-		for m in range(M):
-			data =  map(int , sys.stdin.readline().strip().split())
-			state = data[0]
-			q = data[1]
-			
-			if q == 1:
-				u = data[2] - 1
-				x = data[3]
-				
-				
-			else:
-				u = data[2] - 1
+	N, M = map( int, sys.stdin.readline().strip().split()) 
+	
+	nums = [0]*10
+	
+	for i in range(10):
+		nums[i] = [0]*N
+	
+	A = [0] * N
+	
+	
+	for n in xrange(N):
+		for i in range(10):
+			nums[i][n] = []
+		j = 0
+		for k in  map( int, sys.stdin.readline().strip().split()) :
+			nums[k][n].append(j)
+			j += 1
+	
+	magicTree = [0]*10
+	_mul = N * M* (int(math.log(N*M,2))+3) ##??
+	for i in range(10):
+		magicTree[i] = [0]*_mul
+		build_tree(magicTree[i],nums[i],0,0,N-1)
+	
+	
+	#print nums[0]
+	#print magicTree[0]
+	
+	print getDict(magicTree[0], N, 0,1,0,2)  # top_i, bot_i, top_j, bot_j
+	
+	# _mul = N * (int(math.log(N,2))+3)
+	# magicTree = [0]*_mul
+	
+	
+	# build_tree(magicTree,A,0,0,N-1,M)
+	
+	# print getDict(magicTree, N, 0,N-1,0,M-1)
 
-		
-		
- 
-		
- 
-		
+	
+	
+	
 def test(A,L,R,X,Y):
 	1
 	
 def generate():
-	1
+	M = 10**5
+	N = 1
+	print N, M
+	
+	
+	for n in range(N):
+		_str = ""
+		for m in range(M):
+			_str += "0 "
+		print _str
 	
 	
 if __name__ == "__main__":
 	
-	s= time.time()
-	
-	main()
 	#generate()
-	#test()
 	
- 
+	s= time.time()
+	main()
 	print time.time()-s   
+	
+	#test()
