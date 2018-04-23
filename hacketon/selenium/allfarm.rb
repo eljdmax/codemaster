cd4cfd515ceb6c14f74a5b2c9f097a137f9ed08f
robvm <rob@vm.com>
2018-04-23 16:05:24 +0100

    back to b

--- hacketon/selenium/allfarm.rb
+++ hacketon/selenium/allfarm.rb
@@ -5,6 +5,8 @@ $inputFile = '/tmp/mt0'
 $apiKey = ''
 $domain = '@gmail.com'
 
+
+
 if ARGV[0] then
   $apiKey = ARGV[0]
 end
@@ -24,12 +26,12 @@ $xalphan = 'abcdefghijklmnopqrstuvwzxy0123456789'
 $la = $alpha.length - 1
 $lx = $xalphan.length - 1
 
-$http = Net::HTTP.new('api.hippoapi.com',443)
-$http.use_ssl = true
+##$http = Net::HTTP.new('api.hippoapi.com',443)
+##$http.use_ssl = true
+$http = Net::HTTP.new('apilayer.net',80)
 
 
-#def isUsed(account)
-def isUsed(apiKey,account)
+def processHippo(apiKey,account)
 
   path = '/v3/more/json/'+apiKey+'/'+account+$domain
   header = {   
@@ -46,23 +48,64 @@ def isUsed(apiKey,account)
   end
 
   #puts content
-  if content[0] == '{' then
-     puts apiKey
+  begin
+     ##puts apiKey
      dCont = JSON.parse(content)
      puts dCont["hippoTrust"]["score"] 
-     #return (dCont["hippoTrust"]["score"] > 1)
+     return (dCont["hippoTrust"]["score"] > 1)
+  rescue Exception => e
+     print "Error: "+content
+     exit(2)
   end
-  return false
 
 end
 
-left = ("%04X" % $apiKey)
-(9871..65535).each do |i|
+
+def processMailBox(apiKey,account)
+
+  path = '/api/check?access_key='+apiKey+'&email='+account+$domain+'&smtp=1&format=1'
+  header = {
+               "Accept" => "*/*",
+               "Accept-Language" => "en-US,en;q=0.5",
+               "Connection" => "keep-alive",
+               "Content-type" => "application/json",
+               "Host" => "apilayer.net"
+  }
+
+  content = ''
+  resp = $http.get(path,header) do |str|
+     content += str
+  end
+
+  begin
+     ##puts apiKey
+     dCont = JSON.parse(content)
+     puts dCont["score"]
+     return (dCont["score"] > 0.74)
+  rescue  Exception => e
+     puts "Error: "+content
+     exit(2)
+  end
+
+end
+
+
+def isUsed(apiKey,account)
+   #return processHippo(apiKey,account)
+    return processMailBox(apiKey,account)
+end
+
+=begin
+#left = ("%04X" % $apiKey)
+(2..8).each do |k|
+  left = ("%04X" % k)
+  (0..65535).each do |i|
     key = left +  ("%04X" % i)
     isUsed(key, "richard.d3")
-    sleep(0.1)
+    sleep(0.125)
+  end
 end
-
+=end
 
 def output(line)
 
@@ -78,6 +121,11 @@ def output(line)
 end
 
 =begin
+    print "spencer.q7" + "\t\t"
+    if isUsed($apiKey, "spencer.q7") then
+      output("spencer.q7")
+    end
+=end
 
 $lines = File.readlines($inputFile)
 
@@ -86,14 +134,12 @@ begin
   $lines.each do |line|
     $last = line.chomp
     print $last + "\t\t"
-    if isUsed($last) then
+    if isUsed($apiKey, $last) then
       output($last)
     end
-    sleep(0.25)
+    sleep(0.2)
   end
 
 rescue Exception => e
   puts "Stopped at #{$last} because of : #{e}"
 end
-
-=end
